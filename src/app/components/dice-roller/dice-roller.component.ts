cce89306621170f3d0bea652ffdcc442d8dd7349
Coleman <ckirbyee92@gmail.com>
2020-06-14 16:34:55 -0400

    displaying random numbers while roll is happening

--- src/app/components/dice-roller/dice-roller.component.ts
+++ src/app/components/dice-roller/dice-roller.component.ts
@@ -14,20 +14,29 @@ export class DiceRollerComponent implements OnInit {
   ngOnInit(): void {
   }
 
-  rollDice(): void {
+  async rollDice() {
+
+    for(var i=0; i<10; i++) {
+      this.currentRoll = this.generateRandomNumber(1,21)
+      await this.delay(100);
+
+    }
 
     this.currentRoll = this.generateRandomNumber(1,21);
     this.storeHistory(this.currentRoll)
   }
 
 
-  generateRandomNumber(min, max){
+  generateRandomNumber(min, max): number{
     return Math.floor(Math.random() * (max - min) + min);
   }
 
   storeHistory(currentRoll): void {
     this.rollHistory.push(currentRoll)
   }
-
+  
+  delay(ms: number) {
+    return new Promise( resolve => setTimeout(resolve, ms) );
+}
 }
 
