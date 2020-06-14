cbce4834eddce4e83cb27b487a9724cbc278bc26
Superlimen <wornwing@gmail.com>
2020-06-14 14:50:02 -0500

    no i cant

--- src/app/components/dice-roller/dice-roller.component.ts
+++ src/app/components/dice-roller/dice-roller.component.ts
@@ -7,9 +7,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DiceRollerComponent implements OnInit {
 
+  public currentRoll = 0;
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  rollDice(): void {
+
+    this.currentRoll = this.generateRandomNumber(1,21);
+  }
+
+
+  generateRandomNumber(min, max){
+    return Math.floor(Math.random() * (max - min) + min);
+}
 }
