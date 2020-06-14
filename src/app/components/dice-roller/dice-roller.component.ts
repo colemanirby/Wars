fed14bf498c5b0f455b5e03107496b99abbdfb95
Coleman <ckirbyee92@gmail.com>
2020-06-14 15:25:05 -0400

    Don't have autosave on VSCode so missed some files

--- src/app/components/dice-roller/dice-roller.component.ts
+++ src/app/components/dice-roller/dice-roller.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-dice-roller',
+  selector: 'dice-roller',
   templateUrl: './dice-roller.component.html',
   styleUrls: ['./dice-roller.component.css']
 })
